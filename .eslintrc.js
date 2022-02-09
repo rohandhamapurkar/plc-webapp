1ef38326424a77f25fd085a39ab3987ce1c280b2
Rohan Dhamapurkar <dhamapurkar54@gmail.com>
2022-02-09 08:56:12 +0530

    other: eslint rule ignored

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,5 +12,6 @@ module.exports = {
 		"no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
 		"no-unused-vars": "off",
 		"no-prototype-builtins": "off",
+		"vue/multi-word-component-names": "off",
 	},
 };
